923686ffcfb3f7d4d0624385d31587427118bbb5
desmati@gmail.com <desmati@live.com>
2022-10-03 00:50:51 +0200

    created layout

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+var body = document.getElementsByTagName('body')[0];
+var button = document.getElementById('btn-contrast');
+button.addEventListener('click', function (e) {
+    if (body.classList.contains('hide-images')) {
+        body.classList.remove("hide-images");
+        button.innerHTML = 'High Contrast';
+    } else {
+        body.classList.add("hide-images");
+        button.innerHTML = 'Normal';
+    }
+
+});
+
+var sections = document.querySelectorAll('.section');
+sections.forEach(function (elm) {
+    elm.addEventListener('mouseover', function (e) {
+        sections.forEach(function (elm) {
+            elm.classList.remove('active');
+        });
+    });
+});
+
+
+
+
